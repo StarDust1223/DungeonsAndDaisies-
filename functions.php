1ee14950d6619d016e099e33dda957b2163fbf15
StarDust <astaeste@gmail.com>
2022-03-24 15:16:31 +0200

    Creat functie pentru form

--- functions.php
+++ functions.php
@@ -18,4 +18,23 @@ function finish($next){
 function viata(){
     echo "viata ta este: " . $_SESSION["hp"];
 }
+
+
+/** 
+ * @Input options type Array
+ */
+function formular($options){
+// ["ataci", "fugi", "ascunzi"]
+
+    echo "<form action='' method='POST'>"; //deschidem formular
+    echo "<label for=\"cefaci\">Ce faci? </label>";
+    echo "<select name=\"cefaci\" id=\"cefaci\" class=\"form-control\">";    
+foreach($options as $option){
+    echo "<option value=\"$option\">$option</option>";
+}
+    echo "</select>";
+    echo "<button class=\"btn btn-info mt-3\" type=\"submit\">Actioneaza!</button>";
+    echo "</form>"; //inchidem formular
+
+}
 ?>
