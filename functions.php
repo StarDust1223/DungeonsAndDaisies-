e1e2ddff03d2ebd10349ed5a5657a1aac92b25b2
StarDust <astaeste@gmail.com>
2022-03-24 16:01:33 +0200

    creat functia imagine si proceseaza

--- functions.php
+++ functions.php
@@ -29,12 +29,32 @@ function formular($options){
     echo "<form action='' method='POST'>"; //deschidem formular
     echo "<label for=\"cefaci\">Ce faci? </label>";
     echo "<select name=\"cefaci\" id=\"cefaci\" class=\"form-control\">";    
-foreach($options as $option){
-    echo "<option value=\"$option\">$option</option>";
-}
+        foreach($options as $option){
+         echo "<option value=\"$option\">$option</option>";
+        }
     echo "</select>";
     echo "<button class=\"btn btn-info mt-3\" type=\"submit\">Actioneaza!</button>";
     echo "</form>"; //inchidem formular
 
+}
+
+function imagine($imagine, $alt="imagine"){
+    echo "<img class=\"img img-responsive\" src=\"../images/$imagine\" alt=\"$alt\">";
+} 
+
+function proceseaza($actions, $capitol, $nextCap){
+    $request = isset($_POST)?$_POST:null;
+    if($request) {
+        $_SESSION[$capitol] = $request;
+        if(isset($request['cefaci'])){
+            $_SESSION[$capitol]['cefaci'] = $request['cefaci'];
+            $_SESSION[$capitol]['damage'] = $actions[$request['cefaci']];
+        } else {
+            // de verificat ce s-a intamplat
+        }
+        finish("../$nextCap");
+    }
+
+    
 }
 ?>
